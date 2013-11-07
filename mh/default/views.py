7a1166013fe833fa8ba4cd2c5bb267fce7605d63
Andriy Gushuley <agushuley@me.com>
2013-11-07 09:42:04 +0200

    Test cases

--- mh/default/views.py
+++ mh/default/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from django.template import Context
+from gushuley.multihost import mh_utils
 
 
 def news_item(ctx, site_name, item_id):
@@ -9,5 +11,8 @@ def news_item(ctx, site_name, item_id):
     return response
 
 
-def templates(ctx):
-    return render_to_response("templates.html")
+def templates(request):
+    return render_to_response("templates.html",
+                              Context({'default_site': mh_utils.get_default_site(),
+                                       'current_site': mh_utils.get_current_site(), })
+    )
